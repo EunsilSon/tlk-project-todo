25c8114ea1491539017ad2beb976a5613abbfa18
leetangle <adszzz11@gmail.com>
2025-03-13 20:56:23 +0900

    📝  [issue-review-001] 과제1. @SpringBootApplication만 달면 현업에서도 잘 될까?

--- todo/src/main/java/project/crud/todo/TodoApplication.java
+++ todo/src/main/java/project/crud/todo/TodoApplication.java
@@ -3,6 +3,10 @@ package project.crud.todo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 여기에 repository, entity 몇 depth까지 내려가야 인식 안 되는지 테스트 후 알려주기
+ * ComponentScan은 어느 때에 달아줘야하는지 테스트 후 알려주기
+ */
 @SpringBootApplication
 public class TodoApplication {
 
